4e008ac3a8c3f745d80801d7f4af4cc8a3e1f469
Dr. Gabriel Guillen <ing.guillen.92@gmail.com>
2023-02-26 17:10:49 -0500

    Final commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,8 +62,45 @@ public_users.get('/review/:isbn',function (req, res) {
     }
 });
 
+// TASK 10: Get the book list available in the shop
+public_users.get('/books',function(req,res){
+    const book_promise =new Promise((resolve,reject)=>{
+        resolve(res.send(books));
+    })
+    book_promise.then("The promise for TASK 10 was solved")
+});
 
 
+// TASK 11: Get book details based on ISBN
+public_users.get('/books/isbn/:isbn',function (req, res) {
+    const book_promise =new Promise((resolve,reject)=>{
+        resolve(res.send(books[req.params.isbn]))
+    });
+    book_promise.then("The promise for TASK 11 was solved")
+});
 
+// TASK 12:  Get book details based on author
+public_users.get('/books/author/:author',function (req, res) {
+    const book_promise =new Promise((resolve,reject)=>{
+        let filteredBooks = Object.values(books).filter((book)=>{
+            return (book.author == req.params.author)
+        });
 
+        resolve(res.send(filteredBooks));
+    });
+    book_promise.then("The promise for TASK 12 was solved")
+
+});
+  
+  //TASK 13:  Get all books based on title
+  public_users.get('/books/title/:title',function (req, res) {
+    const book_promise =new Promise((resolve,reject)=>{
+        let filteredBooks = Object.values(books).filter((book)=>{
+          return (book.title == req.params.title)
+        });
+        resolve(res.send(filteredBooks));
+    });
+    book_promise.then("The promise for TASK 13 was solved");
+    });
+    
 module.exports.general = public_users;
